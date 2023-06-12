06f67e9c5e357ad6ef21fbae33421ae8d393e2d4
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-06-12 10:03:10 +0700

    [FEATURE] create cuts feature

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String[] bamboos;
+        int cutCycles;
 
         System.out.print("bamboos: ");
         bamboos = scan.nextLine().split(",");
+        System.out.print("cuts: ");
+        cutCycles = scan.nextInt();
 
         System.out.println("initials:");
         for (String bamboo : bamboos) {
@@ -15,6 +18,18 @@ public class Main {
                 System.out.print("-");
             }
             System.out.println("");
+        }
+
+        for (int i=0; i<cutCycles; i++) {
+            System.out.printf("cycle cuts: %d%n", i+1);
+
+            for (String bamboo : bamboos) {
+                System.out.print("|");
+                for (int j=0; j<Integer.parseInt(bamboo)-(i+1); j++) {
+                    System.out.print("-");
+                }
+                System.out.println("");
+            }
         }
     }
 }
