0f7fe5903b829dfc909fe630fdd1670b755ac622
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-06-12 09:29:16 +0700

    [FEATURE] create input bamboos feature from user

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] bamboos;
+
+        System.out.print("bamboos: ");
+        bamboos = scan.nextLine().split(",");
+
+    }
+}
